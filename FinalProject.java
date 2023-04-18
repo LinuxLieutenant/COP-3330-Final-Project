490660b900bd5ea49ed43a59a1e1d5017fe0c908
Logan McBride <loganm7972@gmail.com>
2023-04-18 09:48:55 -0400

    Fix file checking and add idexception

--- FinalProject.java
+++ FinalProject.java
@@ -3,9 +3,7 @@
 - Logan McBride, Lily Yarbrough
 - (optional) Add anything that you would like the TA to be aware of
 */
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -14,15 +12,29 @@ public class FinalProject {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Enter the absolute path of the file: ");
-		String input = scanner.nextLine();
-		try{
+		while (true) {
+			String input = scanner.nextLine();
 			File file = new File(input);
-		}catch(IOException e) {
-			System.out.println("Sorry no such file");
-			e.printStackTrace();
+        	if (file.exists()) {
+            	System.out.println("File Found! Let's proceed...");
+            	break;
+        	} else {
+            	System.out.println("Sorry no such file.\nTry again:");
+        	}
 		}
-		System.out.println("test");
+		System.out.println("*******************************************");
+		
+		
+		
+		
+		
+		
 		scanner.close();
 	}
 
+}
+class IdException extends Exception{
+	public IdException() {
+		super("Sorry, incorrect format. (IDs are 7 digits)");
+	}
 }
