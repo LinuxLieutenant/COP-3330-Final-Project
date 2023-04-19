90a280f24b3c0e1175ff195d527b01deb57d0669
lemonyy11 <125504096+lemonyy11@users.noreply.github.com>
2023-04-18 21:42:16 -0400

    Add files via upload
    contains main menu (21:42 4/18)

--- FinalProject.java
+++ FinalProject.java
@@ -3,38 +3,40 @@
 - Logan McBride, Lily Yarbrough
 - (optional) Add anything that you would like the TA to be aware of
 */
-import java.io.*;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class FinalProject {
+	static void MainMenu() { //holds the menu, I decided against doing 7 print lines as he said to avoid redundancy 
+		System.out.println("*****************************************\n"
+				+ "1- Add a new Faculty to the schedule\n"
+				+ "2- Enroll a Student to a Lecture\n"
+				+ "3- Print the schedule of a Faculty\n"
+				+ "4- Print the schedule of an TA\n"
+				+ "5- Print the schedule of a Student\n"
+				+ "6- Delete a Lecture\n"
+				+ "7- Exit\n");
+	}
 	
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Enter the absolute path of the file: ");
-		while (true) {
+		
+		while(true) {
 			String input = scanner.nextLine();
 			File file = new File(input);
-        	if (file.exists()) {
-            	System.out.println("File Found! Let's proceed...");
-            	break;
-        	} else {
-            	System.out.println("Sorry no such file.\nTry again:");
-        	}
+			if(file.exists()) {
+				System.out.println("File found! Let's proceed...");
+			}
+			else
+				System.out.print("Sorry no such file.\nTry again:");
 		}
-		System.out.println("*******************************************");
 		
-		
-		
-		
-		
-		
+
 		scanner.close();
 	}
-
-}
-class IdException extends Exception{
-	public IdException() {
-		super("Sorry, incorrect format. (IDs are 7 digits)");
-	}
+	
 }
